cf21e9e28c81bfd56978b1d7c1ca477abc20b832
Kaled Aljebur <kaledaljebur@gmail.com>
2025-05-29 17:46:15 +1000

    Script update

--- bind9-dns-install.sh
+++ bind9-dns-install.sh
@@ -2,51 +2,13 @@
 
 echo "This script is created by Kaled Aljebur to Install Bind9 DNS server."
 echo "This also include enabling Netplan and brief IP and DNS setup."
-#Disable NetworkManager
-echo "Disable NetworkManager..."
-sudo systemctl stop NetworkManager
-sudo systemctl disable NetworkManager
 
-#Enable systemd-networkd to manage networking
-echo "Enabling systemd-networkd for network management..."
-sudo systemctl enable systemd-networkd
-sudo systemctl start systemd-networkd
+# echo "***********************************"
+# echo "Update APT..."
+# echo "***********************************"
 
-#Rename the available profile, unlike NetworManager, Netplan only working with .yaml, not .yml
-sudo mv /etc/netplan/01-network-manager-all.yml /etc/netplan/01-network-manager-all.yaml
+# sudo apt update
 
-#Edit the Netplan YAML profile
-echo "Create Netplan YAML progfile..."
-sudo tee /etc/netplan/01-network-manager-all.yaml > /dev/null <<EOF
-# Create by Kaled Aljebur as a sample and tested in VMware environment.
-network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    ens33:
-      dhcp4: false
-      addresses:
-        - 192.168.8.50/24
-      gateway4: 192.168.8.2
-      nameservers:
-        addresses:
-          - 192.168.8.2
-          # - 1.1.1.1
-EOF
-
-#Apply the Netplan profile
-echo "Apply the Netplan profile..."
-sudo netplan apply
-
-#Pause to get the changes applied
-echo "Waiting the changes to be applied..."
-sleep 3
-
-#List the new IP settings
-echo "List the new IP settings..."
-ip a sh ens33
-
-sleep 2
 echo "***********************************"
 echo "Installing bind9 will start soon..."
 echo "***********************************"
@@ -115,7 +77,53 @@ options {
 };
 EOF
 
-#Start and and auto start bind9 after reboot
+#Disable NetworkManager
+echo "Disable NetworkManager..."
+sudo systemctl stop NetworkManager
+sudo systemctl disable NetworkManager
+
+#Enable systemd-networkd to manage networking
+echo "Enabling systemd-networkd for network management..."
+sudo systemctl enable systemd-networkd
+sudo systemctl start systemd-networkd
+
+#Rename the available profile, unlike NetworManager, Netplan only working with .yaml, not .yml
+sudo mv /etc/netplan/01-network-manager-all.yml /etc/netplan/01-network-manager-all.yaml
+
+#Edit the Netplan YAML profile
+echo "Create Netplan YAML progfile..."
+sudo tee /etc/netplan/01-network-manager-all.yaml > /dev/null <<EOF
+# Create by Kaled Aljebur as a sample and tested in VMware environment.
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    ens33:
+      dhcp4: false
+      addresses:
+        - 192.168.8.50/24
+      gateway4: 192.168.8.2
+      nameservers:
+        addresses:
+          - 192.168.8.2
+          # - 1.1.1.1
+EOF
+
+#Apply the Netplan profile
+echo "Apply the Netplan profile..."
+sudo netplan apply
+
+#Pause to get the changes applied
+echo "Waiting the changes to be applied..."
+sleep 3
+
+#List the new IP settings
+echo "List the new IP settings..."
+ip a sh ens33
+
+# sleep 2
+
+#Start and enable bind9
 sudo systemctl start bind9
 sudo systemctl enable named
 
